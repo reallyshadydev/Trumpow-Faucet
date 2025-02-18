6064b3f0f5121cb2826be24bc21a0151d4e1d880
TechnoVisionDev <thomasanthonypeters@gmail.com>
2025-02-17 21:04:30 -0800

    Add favicon and tab name

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import React, { useState, useRef } from 'react';
 import HCaptcha from '@hcaptcha/react-hcaptcha';
 import { callFlopcoin } from '../lib/flopcoinRPC';
 import Navbar from '@/components/Navbar';
+import Head from 'next/head';
 
 export default function Home({ faucetBalance, totalPaidOut, numPayouts }) {
   const [address, setAddress] = useState('');
@@ -50,98 +51,112 @@ export default function Home({ faucetBalance, totalPaidOut, numPayouts }) {
   };
 
   return (
-    <div className="min-h-screen flex flex-col bg-[#212121] text-white">
-      {/* Navbar */}
-      <Navbar />
+    <>
+      <Head>
+        <title>Flopcoin Faucet</title>
+        {/* Standard favicon */}
+        <link rel="icon" href="/favicon.ico" />
 
-      {/* Main Content */}
-      <main className="flex-grow flex justify-center">
-        <div className="w-full max-w-2xl px-6 sm:px-4 p-1 pt-0">
-          {/* Banner Image */}
-          <div className="w-full">
-            <div
-              className="mx-auto mt-2"
-              style={{ transform: 'scale(1.2)', transformOrigin: 'center' }}
-            >
-              <img src="/banner.png" alt="Banner" className="w-auto h-auto mt-12" />
-            </div>
-          </div>
+        {/* For Apple devices */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
-          {/* Stats Block */}
-          <div className="flex justify-center mt-1">
-            <div className="text-left text-gray-300 space-y-1 mb-4 text-base leading-normal">
-              <p className="flex items-center text-xl">
-                <span className="mr-2 text-xl">⏰</span>
-                <span>
-                  You can claim <b>100 FLOP</b> every hour.
-                </span>
-              </p>
-              <p className="flex items-center text-xl">
-                <span className="mr-2">🏦</span>
-                <span>
-                  Faucet Balance: <b>{faucetBalance} FLOP</b>
-                </span>
-              </p>
-              <p className="flex items-center text-xl">
-                <span className="mr-2 text-xl">💰</span>
-                <span>
-                  Total Paid Out: <b>{totalPaidOut} FLOP</b>
-                </span>
-              </p>
-              <p className="flex items-center text-xl">
-                <span className="mr-2 text-xl">❤️</span>
-                <span>
-                  <a href="/donate" className="underline text-[#e979be] hover:text-[#e979be]">
-                    Donate
-                  </a>{' '}
-                  to keep the faucet running
-                </span>
-              </p>
-            </div>
-          </div>
+        {/* For Android Chrome */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </Head>
+      <div className="min-h-screen flex flex-col bg-[#212121] text-white">
+        {/* Navbar */}
+        <Navbar />
 
-          {/* Claim Form */}
-          <div className="mt-6">
-            <div className="flex items-center space-x-2 mb-2">
-              <input
-                type="text"
-                placeholder="Enter your FLOP address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                className="border border-gray-600 bg-gray-700 text-white rounded placeholder-gray-400 h-14 w-[600px] pl-4 pr-2"
-              />
-              <button
-                onClick={handleClaim}
-                disabled={!address || !hcaptchaToken || isClaiming}
-                className="bg-[#df3da1] text-white px-4 h-14 rounded disabled:opacity-50 hover:bg-[#ce228c] flex items-center justify-center"
-              >
-                {isClaiming ? 'Processing...' : 'Submit'}
-              </button>
-            </div>
-
-            {/* hCaptcha */}
-            <div className="mt-8 flex justify-center">
-              <HCaptcha
-                ref={hcaptchaRef}
-                sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
-                onVerify={onVerifyCaptcha}
-              />
-            </div>
-
-            {/* Message */}
-            {message && (
+        {/* Main Content */}
+        <main className="flex-grow flex justify-center">
+          <div className="w-full max-w-2xl px-6 sm:px-4 p-1 pt-0">
+            {/* Banner Image */}
+            <div className="w-full">
               <div
-                className={`mt-4 font-semibold text-center ${
-                  message.startsWith('Success') ? 'text-green-400' : 'text-red-400'
-                }`}
+                className="mx-auto mt-2"
+                style={{ transform: 'scale(1.2)', transformOrigin: 'center' }}
               >
-                {message}
+                <img src="/banner.png" alt="Banner" className="w-auto h-auto mt-12" />
               </div>
-            )}
+            </div>
+
+            {/* Stats Block */}
+            <div className="flex justify-center mt-1">
+              <div className="text-left text-gray-300 space-y-1 mb-4 text-base leading-normal">
+                <p className="flex items-center text-xl">
+                  <span className="mr-2 text-xl">⏰</span>
+                  <span>
+                    You can claim <b>100 FLOP</b> every hour.
+                  </span>
+                </p>
+                <p className="flex items-center text-xl">
+                  <span className="mr-2">🏦</span>
+                  <span>
+                    Faucet Balance: <b>{faucetBalance} FLOP</b>
+                  </span>
+                </p>
+                <p className="flex items-center text-xl">
+                  <span className="mr-2 text-xl">💰</span>
+                  <span>
+                    Total Paid Out: <b>{totalPaidOut} FLOP</b>
+                  </span>
+                </p>
+                <p className="flex items-center text-xl">
+                  <span className="mr-2 text-xl">❤️</span>
+                  <span>
+                    <a href="/donate" className="underline text-[#e979be] hover:text-[#e979be]">
+                      Donate
+                    </a>{' '}
+                    to keep the faucet running
+                  </span>
+                </p>
+              </div>
+            </div>
+
+            {/* Claim Form */}
+            <div className="mt-6">
+              <div className="flex items-center space-x-2 mb-2">
+                <input
+                  type="text"
+                  placeholder="Enter your FLOP address"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  className="border border-gray-600 bg-gray-700 text-white rounded placeholder-gray-400 h-14 w-[600px] pl-4 pr-2"
+                />
+                <button
+                  onClick={handleClaim}
+                  disabled={!address || !hcaptchaToken || isClaiming}
+                  className="bg-[#df3da1] text-white px-4 h-14 rounded disabled:opacity-50 hover:bg-[#ce228c] flex items-center justify-center"
+                >
+                  {isClaiming ? 'Processing...' : 'Submit'}
+                </button>
+              </div>
+
+              {/* hCaptcha */}
+              <div className="mt-8 flex justify-center">
+                <HCaptcha
+                  ref={hcaptchaRef}
+                  sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
+                  onVerify={onVerifyCaptcha}
+                />
+              </div>
+
+              {/* Message */}
+              {message && (
+                <div
+                  className={`mt-4 font-semibold text-center ${
+                    message.startsWith('Success') ? 'text-green-400' : 'text-red-400'
+                  }`}
+                >
+                  {message}
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
 
